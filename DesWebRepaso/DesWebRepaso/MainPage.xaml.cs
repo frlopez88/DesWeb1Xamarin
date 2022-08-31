cca030347edaac32f0a3973414592db1523180c2
frlopez88 <68876977+frlopez88@users.noreply.github.com>
2022-08-30 20:46:29 -0600

    Poo
    Creacion de Modelos

--- DesWebRepaso/DesWebRepaso/MainPage.xaml.cs
+++ DesWebRepaso/DesWebRepaso/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DesWebRepaso.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -30,6 +31,20 @@ namespace DesWebRepaso
             etiqueta1.Text = "Hola soy Mario";
             etiqueta1.TextColor = Xamarin.Forms.Color.FromHex("#eb2d1c");
             imagen1.Source = "https://assets.nintendo.com/image/upload/f_auto/q_auto/dpr_1.25/c_scale,w_600/ncom/en_US/games/switch/n/new-super-mario-bros-u-deluxe-switch/description-image";
+        }
+
+        private void FuncionCrearPersona(object sender, EventArgs e)
+        {
+            persona temporal = new persona() { 
+                nombre = nombre_form.Text,
+                edad = int.Parse(edad_form.Text), 
+                pass = pass_form.Text, 
+                telefono = telefono_form.Text
+            };
+
+
+            Resultado.Text = temporal.reportePersona();
+
         }
     }
 }
---
+++ DesWebRepaso/DesWebRepaso/Models/persona.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesWebRepaso.Models
+{
+    public class persona
+    {
+
+        public string nombre {get; set;}
+        public int edad { get; set; }
+        public string telefono { get; set; }
+        public string pass { get; set; }
+
+
+        public string reportePersona() {
+
+            return "No nombre es: " + nombre + " Mi telefono es: " + telefono;
+
+
+        }
+
+    }
+}
