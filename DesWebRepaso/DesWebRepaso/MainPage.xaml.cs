ebd4c64478a513bb7588279e83122710df5f9f52
frlopez88 <68876977+frlopez88@users.noreply.github.com>
2022-08-30 20:10:29 -0600

    Manejo de Eventos
    Manejo de funciones y Botones

--- DesWebRepaso/DesWebRepaso/MainPage.xaml.cs
+++ DesWebRepaso/DesWebRepaso/MainPage.xaml.cs
@@ -14,5 +14,22 @@ namespace DesWebRepaso
         {
             InitializeComponent();
         }
+
+        private void FuncionLuigi(object sender, EventArgs e)
+        {
+
+            etiqueta1.Text = "Hola soy Luigi";
+            etiqueta1.TextColor = Xamarin.Forms.Color.FromHex("#15e665");
+            imagen1.Source = "https://www.nintenderos.com/wp-content/uploads/2009/08/luigi-nintendo.jpg";
+
+
+        }
+
+        private void FuncioMario(object sender, EventArgs e)
+        {
+            etiqueta1.Text = "Hola soy Mario";
+            etiqueta1.TextColor = Xamarin.Forms.Color.FromHex("#eb2d1c");
+            imagen1.Source = "https://assets.nintendo.com/image/upload/f_auto/q_auto/dpr_1.25/c_scale,w_600/ncom/en_US/games/switch/n/new-super-mario-bros-u-deluxe-switch/description-image";
+        }
     }
 }
